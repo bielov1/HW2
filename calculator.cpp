769764768ea7a75c147745a1ee45c2a5da1cff54
Oleh Bielov <olaph22@gmail.com>
2023-10-26 18:36:10 +0300

    added some code

--- calculator.cpp
+++ calculator.cpp
@@ -12,5 +12,21 @@ int Calculator::Sub (double a, double b)
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+	return a * b + 0.5;
+}
+
+int Calculator::Div (double a, double b)
+{
+	return a / b;
+}
+
+int Calculator::Pow (double a, double b)
+{
+	int res = 1;
+	while(b > 0){
+		res *= a;
+		b--;
+	}
+
+	return res;
 }
